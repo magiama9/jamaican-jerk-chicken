7c33c08916ffef60dc89b085585305a3d327f606
magiama9 <58517943+magiama9@users.noreply.github.com>
2020-02-03 13:21:12 -0500

    Add basic post functionality and install node dependencies

---
+++ server.js
@@ -0,0 +1,50 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// Reservation Table Storage
+
+const tables = [];
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// Displays all characters
+app.get("/api/reserve", function(req, res) {
+  return res.json(characters);
+});
+
+// Create New Reservation - takes in JSON input
+app.post("/api/reserve", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  const newResy = req.body;
+
+  console.log(newResy);
+
+  tables.push(newResy);
+
+  res.json(tables);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
